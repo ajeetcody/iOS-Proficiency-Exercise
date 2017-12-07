bf2d4aca8f48dc216f70bd979e1d987ad0352d86
ajeetcody <ajeet.cody@gmail.com>
2017-12-08 01:36:37 +0530

    Inital Commit

--- Proficiency Exercise/AppDelegate.swift
+++ Proficiency Exercise/AppDelegate.swift
@@ -13,9 +13,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var viewController: ViewController?
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        self.window?.rootViewController = viewController
+        
+        self.window?.backgroundColor = UIColor.white
+
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
