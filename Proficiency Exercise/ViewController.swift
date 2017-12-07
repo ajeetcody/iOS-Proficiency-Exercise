074d1d33809ee4dd13fb9098f900761da53d146b
Ajeet Sharma <ajeetsharma@ajeets-MacBook-Pro.local>
2017-12-08 01:03:48 +0530

    Initial Commit

---
+++ Proficiency Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proficiency Exercise
+//
+//  Created by ajeet sharma on 08/12/17.
+//  Copyright © 2017 Ajeet Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
